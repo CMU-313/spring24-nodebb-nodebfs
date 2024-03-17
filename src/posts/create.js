878179724b96f9cf3486e9254961a0d42b18c02f
Ao Li <leeleo3x@gmail.com>
2024-03-17 16:15:38 -0400

    Draft PR for translating post contents in English.

--- src/posts/create.js
+++ src/posts/create.js
@@ -10,14 +10,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const _ = require("lodash");
-const meta = require("../meta");
-const db = require("../database");
-const plugins = require("../plugins");
-const user = require("../user");
-const topics = require("../topics");
-const categories = require("../categories");
-const groups = require("../groups");
-const utils = require("../utils");
+const _ = require('lodash');
+
+const meta = require('../meta');
+const db = require('../database');
+const plugins = require('../plugins');
+const user = require('../user');
+const topics = require('../topics');
+const categories = require('../categories');
+const groups = require('../groups');
+const utils = require('../utils');
+const translate = require('../translate');
+'use strict';
+
 module.exports = function (Posts) {
     function addReplyTo(postData, timestamp) {
         return __awaiter(this, void 0, void 0, function* () {
@@ -40,6 +45,8 @@ module.exports = function (Posts) {
         const timestamp = data.timestamp || Date.now();
         const isMain = data.isMain || false;
         const isAnon = data.anonymous;
+        const [isEnglish, translatedContent] = await translate.translate(data)
+
         if (!uid && parseInt(uid, 10) !== 0) {
             throw new Error('[[error:invalid-uid]]');
         }
@@ -55,6 +62,8 @@ module.exports = function (Posts) {
             content: content,
             timestamp: timestamp,
             anonymous: isAnon,
+            translatedContent: translatedContent,
+            isEnglish: isEnglish,
         };
         if (data.toPid) {
             postData.toPid = data.toPid;
