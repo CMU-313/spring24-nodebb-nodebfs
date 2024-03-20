fc556da693fab0d53a43026026fc961c597b9031
TevinWang <tevin.wang@warriorlife.net>
2024-03-20 02:08:42 -0400

    Restore prettier on upload api and remove on typescript generated files

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -10,7 +10,7 @@ const utils = require('../../src/utils');
 
 const helpers = module.exports;
 
-helpers.getCsrfToken = async (jar) => {
+helpers.getCsrfToken = async jar => {
     const { csrf_token: token } = await requestAsync({
         url: `${nconf.get('url')}/api/config`,
         json: true,
@@ -24,7 +24,9 @@ helpers.request = async function (method, uri, options) {
     const ignoreMethods = ['GET', 'HEAD', 'OPTIONS'];
     const lowercaseMethod = String(method).toLowerCase();
     let csrf_token;
-    if (!ignoreMethods.some(method => method.toLowerCase() === lowercaseMethod)) {
+    if (
+        !ignoreMethods.some(method => method.toLowerCase() === lowercaseMethod)
+    ) {
         csrf_token = await helpers.getCsrfToken(options.jar);
     }
 
@@ -33,66 +35,85 @@ helpers.request = async function (method, uri, options) {
         if (csrf_token) {
             options.headers['x-csrf-token'] = csrf_token;
         }
-        request[lowercaseMethod](`${nconf.get('url')}${uri}`, options, (err, res, body) => {
-            if (err) reject(err);
-            else resolve({ res, body });
-        });
+        request[lowercaseMethod](
+            `${nconf.get('url')}${uri}`,
+            options,
+            (err, res, body) => {
+                if (err) reject(err);
+                else resolve({ res, body });
+            },
+        );
     });
 };
 
 helpers.loginUser = function (username, password, callback) {
     const jar = request.jar();
 
-    request({
-        url: `${nconf.get('url')}/api/config`,
-        json: true,
-        jar: jar,
-    }, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-            return callback(err || new Error('[[error:invalid-response]]'));
-        }
-        const { csrf_token } = body;
-        request.post(`${nconf.get('url')}/login`, {
-            form: {
-                username: username,
-                password: password,
-            },
+    request(
+        {
+            url: `${nconf.get('url')}/api/config`,
             json: true,
             jar: jar,
-            headers: {
-                'x-csrf-token': csrf_token,
-            },
-        }, (err, res, body) => {
-            if (err) {
+        },
+        (err, res, body) => {
+            if (err || res.statusCode !== 200) {
                 return callback(err || new Error('[[error:invalid-response]]'));
             }
-            callback(null, { jar, res, body, csrf_token: csrf_token });
-        });
-    });
+            const { csrf_token } = body;
+            request.post(
+                `${nconf.get('url')}/login`,
+                {
+                    form: {
+                        username: username,
+                        password: password,
+                    },
+                    json: true,
+                    jar: jar,
+                    headers: {
+                        'x-csrf-token': csrf_token,
+                    },
+                },
+                (err, res, body) => {
+                    if (err) {
+                        return callback(
+                            err || new Error('[[error:invalid-response]]'),
+                        );
+                    }
+                    callback(null, { jar, res, body, csrf_token: csrf_token });
+                },
+            );
+        },
+    );
 };
 
-
 helpers.logoutUser = function (jar, callback) {
-    request({
-        url: `${nconf.get('url')}/api/config`,
-        json: true,
-        jar: jar,
-    }, (err, response, body) => {
-        if (err) {
-            return callback(err, response, body);
-        }
-
-        request.post(`${nconf.get('url')}/logout`, {
-            form: {},
+    request(
+        {
+            url: `${nconf.get('url')}/api/config`,
             json: true,
             jar: jar,
-            headers: {
-                'x-csrf-token': body.csrf_token,
-            },
-        }, (err, response, body) => {
-            callback(err, response, body);
-        });
-    });
+        },
+        (err, response, body) => {
+            if (err) {
+                return callback(err, response, body);
+            }
+
+            request.post(
+                `${nconf.get('url')}/logout`,
+                {
+                    form: {},
+                    json: true,
+                    jar: jar,
+                    headers: {
+                        'x-csrf-token': body.csrf_token,
+                    },
+                },
+                (err, response, body) => {
+                    callback(err, response, body);
+                },
+            );
+        },
+    );
 };
 
 helpers.connectSocketIO = function (res, callback) {
@@ -112,12 +133,19 @@ helpers.connectSocketIO = function (res, callback) {
         callback(null, socket);
     });
 
-    socket.on('error', (err) => {
+    socket.on('error', err => {
         callback(err);
     });
 };
 
-helpers.uploadFile = function (uploadEndPoint, filePath, body, jar, csrf_token, callback) {
+helpers.uploadFile = function (
+    uploadEndPoint,
+    filePath,
+    body,
+    jar,
+    csrf_token,
+    callback,
+) {
     let formData = {
         files: [
             fs.createReadStream(filePath),
@@ -125,51 +153,61 @@ helpers.uploadFile = function (uploadEndPoint, filePath, body, jar, csrf_token, 
         ],
     };
     formData = utils.merge(formData, body);
-    request.post({
-        url: uploadEndPoint,
-        formData: formData,
-        json: true,
-        jar: jar,
-        headers: {
-            'x-csrf-token': csrf_token,
+    request.post(
+        {
+            url: uploadEndPoint,
+            formData: formData,
+            json: true,
+            jar: jar,
+            headers: {
+                'x-csrf-token': csrf_token,
+            },
         },
-    }, (err, res, body) => {
-        if (err) {
-            return callback(err);
-        }
-        if (res.statusCode !== 200) {
-            winston.error(JSON.stringify(body));
-        }
-        callback(null, res, body);
-    });
+        (err, res, body) => {
+            if (err) {
+                return callback(err);
+            }
+            if (res.statusCode !== 200) {
+                winston.error(JSON.stringify(body));
+            }
+            callback(null, res, body);
+        },
+    );
 };
 
 helpers.registerUser = function (data, callback) {
     const jar = request.jar();
-    request({
-        url: `${nconf.get('url')}/api/config`,
-        json: true,
-        jar: jar,
-    }, (err, response, body) => {
-        if (err) {
-            return callback(err);
-        }
-
-        if (!data.hasOwnProperty('password-confirm')) {
-            data['password-confirm'] = data.password;
-        }
-
-        request.post(`${nconf.get('url')}/register`, {
-            form: data,
+    request(
+        {
+            url: `${nconf.get('url')}/api/config`,
             json: true,
             jar: jar,
-            headers: {
-                'x-csrf-token': body.csrf_token,
-            },
-        }, (err, response, body) => {
-            callback(err, jar, response, body);
-        });
-    });
+        },
+        (err, response, body) => {
+            if (err) {
+                return callback(err);
+            }
+
+            if (!data.hasOwnProperty('password-confirm')) {
+                data['password-confirm'] = data.password;
+            }
+
+            request.post(
+                `${nconf.get('url')}/register`,
+                {
+                    form: data,
+                    json: true,
+                    jar: jar,
+                    headers: {
+                        'x-csrf-token': body.csrf_token,
+                    },
+                },
+                (err, response, body) => {
+                    callback(err, jar, response, body);
+                },
+            );
+        },
+    );
 };
 
 // http://stackoverflow.com/a/14387791/583363
@@ -177,11 +215,11 @@ helpers.copyFile = function (source, target, callback) {
     let cbCalled = false;
 
     const rd = fs.createReadStream(source);
-    rd.on('error', (err) => {
+    rd.on('error', err => {
         done(err);
     });
     const wr = fs.createWriteStream(target);
-    wr.on('error', (err) => {
+    wr.on('error', err => {
         done(err);
     });
     wr.on('close', () => {
@@ -198,16 +236,19 @@ helpers.copyFile = function (source, target, callback) {
 };
 
 helpers.invite = async function (body, uid, jar, csrf_token) {
-    const res = await requestAsync.post(`${nconf.get('url')}/api/v3/users/${uid}/invites`, {
-        jar: jar,
-        // using "form" since client "api" module make requests with "application/x-www-form-urlencoded" content-type
-        form: body,
-        headers: {
-            'x-csrf-token': csrf_token,
+    const res = await requestAsync.post(
+        `${nconf.get('url')}/api/v3/users/${uid}/invites`,
+        {
+            jar: jar,
+            // using "form" since client "api" module make requests with "application/x-www-form-urlencoded" content-type
+            form: body,
+            headers: {
+                'x-csrf-token': csrf_token,
+            },
+            simple: false,
+            resolveWithFullResponse: true,
         },
-        simple: false,
-        resolveWithFullResponse: true,
-    });
+    );
 
     res.body = JSON.parse(res.body);
     return { res, body };
