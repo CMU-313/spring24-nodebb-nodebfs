97dfb7afd9e4b427c041eb5ad52d171511e407a2
jhs-panda <joy22030@gmail.com>
2024-04-01 20:54:25 -0400

    fix lint issues part 2

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,25 +1,9 @@
-'use strict'; 
-
-const fetch = require('node-fetch');
+'use strict'
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    // Ensure the TRANSLATOR_API environment variable is set
-    if (!process.env.TRANSLATOR_API) {
-        throw new Error('TRANSLATOR_API environment variable is not set.');
-    }
-    
-    // Construct the URL using template literals for cleaner syntax
-    const apiUrl = new URL(`/?content=${encodeURIComponent(postData.content)}`, process.env.TRANSLATOR_API);
-    
-    // Make the fetch request
-    const response = await fetch(apiUrl.toString());
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
+    const response = await fetch(`${process.env.TRANSLATOR_API}/?content=${postData.content}`);
     const data = await response.json();
-
-    // Use dot notation to access properties
     return [data.is_english, data.translated_content];
-};
+}
