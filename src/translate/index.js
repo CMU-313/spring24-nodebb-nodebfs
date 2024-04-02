fa9a2546ca0b2c9a95face7b346c236faf07b747
jhs-panda <joy22030@gmail.com>
2024-04-01 20:44:28 -0400

    fix lint issues

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,9 +1,25 @@
-var request = require('request')
+'use strict'; 
+
+const fetch = require('node-fetch');
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    const response = await fetch(process.env.TRANSLATOR_API+'/?content='+postData.content);
+    // Ensure the TRANSLATOR_API environment variable is set
+    if (!process.env.TRANSLATOR_API) {
+        throw new Error('TRANSLATOR_API environment variable is not set.');
+    }
+    
+    // Construct the URL using template literals for cleaner syntax
+    const apiUrl = new URL(`/?content=${encodeURIComponent(postData.content)}`, process.env.TRANSLATOR_API);
+    
+    // Make the fetch request
+    const response = await fetch(apiUrl.toString());
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
     const data = await response.json();
-    return [data["is_english"], data["translated_content"]]
-}
+
+    // Use dot notation to access properties
+    return [data.is_english, data.translated_content];
+};
